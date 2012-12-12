a1d862df0116530fa67dae98861b3a33515e1d8f
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2012-12-12 10:47:06 +0200

    simple hello world server created.

--- src/authserver.c
+++ src/authserver.c
@@ -1,7 +1,50 @@
 #include "authserver.h"
 
-int auth_service(phttp_logger_config config){
-    syslog(LOG_INFO,"auth server is starting\n");
-    sleep(30);
+struct MHD_Daemon * auth_server_daemon = NULL;
+int flag=1;
+
+int auth_service(phttp_logger_config config) {
+    syslog(LOG_INFO, "auth server is starting\n");
+    signal(SIGINT, as_signal_handler);
+    signal(SIGKILL, as_signal_handler);
+    signal(SIGTERM, as_signal_handler);
+
+    auth_server_daemon = MHD_start_daemon(
+            MHD_USE_THREAD_PER_CONNECTION, /* deamon thread option */
+            config->auth_server_port, /* port */
+            NULL, NULL, /* connection filter. accept every client */
+            &answer_to_connection, /* handler function */
+            NULL, /* no parameter for handler */
+            MHD_OPTION_END /* ending options */
+            );
+    if (NULL == auth_server_daemon) return 1;
+    syslog(LOG_INFO, "auth server is started at port %i.\n", config->auth_server_port);
+    while (flag) {
+        sleep(10);
+    }
     return 0;
+}
+
+void as_signal_handler(int sig) {
+    if (auth_server_daemon) {
+        MHD_stop_daemon(auth_server_daemon);
+        flag=0;
+    }
+}
+
+int answer_to_connection(void *cls, struct MHD_Connection *connection,
+        const char *url,
+        const char *method, const char *version,
+        const char *upload_data,
+        size_t *upload_data_size, void **con_cls) {
+    const char *page = "<html><body>Hello, browser!</body></html>";
+    struct MHD_Response *response;
+    int ret;
+
+    response = MHD_create_response_from_buffer(strlen(page),
+            (void*) page, MHD_RESPMEM_PERSISTENT);
+    ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+    MHD_destroy_response(response);
+
+    return ret;
 }
--- src/authserver.h
+++ src/authserver.h
@@ -16,6 +16,12 @@ extern "C" {
 #include <microhttpd.h>
 
     int auth_service(phttp_logger_config config);
+    int answer_to_connection (void *cls, struct MHD_Connection *connection, 
+                          const char *url, 
+                          const char *method, const char *version, 
+                          const char *upload_data, 
+                          size_t *upload_data_size, void **con_cls);
+    void as_signal_handler(int sig);
 #ifdef	__cplusplus
 }
 #endif
